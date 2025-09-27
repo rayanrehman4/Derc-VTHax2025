f29ccf62baf1ffb46959cc5be66712ed8da21531
rayanrehman4 <82070161+rayanrehman4@users.noreply.github.com>
2025-09-27 19:21:29 -0400

    Updated globals.css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import BlueprintTrendline from '../components/BlueprintTrendline';
 
 const testimonials = [
   {
@@ -134,9 +133,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-
-      {/* Blueprint Trendline Visual */}
-      <BlueprintTrendline />
 
       {/* Key Stats */}
       <section className="section-padding bg-gray-900">

059e06fa82f02189848ec8e68eb52360c3c032d9
rayanrehman4 <82070161+rayanrehman4@users.noreply.github.com>
2025-09-27 20:07:59 -0400

    Update Forecasting Page to Use New York Data

--- pages/forecasting.js
+++ pages/forecasting.js
@@ -69,6 +69,7 @@ const CountyMultiSelect = ({ counties = [], selectedCounties = [], onSelectionCh
               className="flex items-center space-x-2 bg-gray-800/50 border border-gray-700 rounded-lg px-3 py-2"
             >
               <span className="text-sm font-medium text-white">{county.region.replace(', CA metro area', '')}</span>
+              <span className="text-sm font-medium text-white">{county.region.replace(', NY metro area', '').replace(', NY', '')}</span>
               <span className="text-xs text-gray-400">{formatCurrency(county[2028])}</span>
               <button
                 onClick={() => handleRemoveCounty(county.region)}
@@ -119,7 +120,7 @@ const CountyMultiSelect = ({ counties = [], selectedCounties = [], onSelectionCh
             >
               <div className="flex items-center justify-between">
                 <div>
-                  <div className="font-medium text-white">{county.region.replace(', CA metro area', '')}</div>
+                  <div className="font-medium text-white">{county.region.replace(', NY metro area', '').replace(', NY', '')}</div>
                   <div className="text-sm text-gray-400">2028 Forecast</div>
                 </div>
                 <div className="text-right">
@@ -204,117 +205,7 @@ const PresetButtons = ({ counties, onPresetSelect }) => {
 };
 
 const californiaData = [
-  { "Region": "Anaheim, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1225293.0714474719 },
-  { "Region": "Anaheim, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1262744.0408474093 },
-  { "Region": "Anaheim, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1310161.8213494173 },
-  { "Region": "Bakersfield, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 393074.8791711438 },
-  { "Region": "Bakersfield, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 404363.5406181416 },
-  { "Region": "Bakersfield, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 415159.9643713332 },
-  { "Region": "Chico, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 415968.1531893862 },
-  { "Region": "Chico, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 418896.4141278189 },
-  { "Region": "Chico, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 424666.9750718521 },
-  { "Region": "Clearlake, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 343578.4322424681 },
-  { "Region": "Clearlake, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 350282.5888563014 },
-  { "Region": "Clearlake, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 355428.7068758621 },
-  { "Region": "Crescent City, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 405736.3789840902 },
-  { "Region": "Crescent City, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 428696.301752826 },
-  { "Region": "Crescent City, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 447407.5241472031 },
-  { "Region": "El Centro, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 411217.7881146441 },
-  { "Region": "El Centro, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 434506.7903439507 },
-  { "Region": "El Centro, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 452747.3790469963 },
-  { "Region": "Eureka, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 429515.876251127 },
-  { "Region": "Eureka, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 435805.1742825092 },
-  { "Region": "Eureka, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 443045.2432760909 },
-  { "Region": "Fresno, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 439658.4004337845 },
-  { "Region": "Fresno, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 452061.3825779539 },
-  { "Region": "Fresno, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 465127.54822495 },
-  { "Region": "Hanford, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 370004.282615847 },
-  { "Region": "Hanford, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 377272.3611673209 },
-  { "Region": "Hanford, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 383408.4649507702 },
-  { "Region": "Los Angeles, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 927590.0748301619 },
-  { "Region": "Los Angeles, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 946272.1781747155 },
-  { "Region": "Los Angeles, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 970252.0799218802 },
-  { "Region": "Madera, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 455482.0091037158 },
-  { "Region": "Madera, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 472374.5287410829 },
-  { "Region": "Madera, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 488226.5958678392 },
-  { "Region": "Merced, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 437900.1169252397 },
-  { "Region": "Merced, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 448448.3716938946 },
-  { "Region": "Merced, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 457737.747176828 },
-  { "Region": "Modesto, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 493448.8740054469 },
-  { "Region": "Modesto, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 505493.9496028612 },
-  { "Region": "Modesto, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 519314.4504302121 },
-  { "Region": "Napa, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 964087.430611582 },
-  { "Region": "Napa, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 988175.6678248585 },
-  { "Region": "Napa, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1022927.126070617 },
-  { "Region": "Oakland, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 928813.1758285064 },
-  { "Region": "Oakland, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 926278.4191810045 },
-  { "Region": "Oakland, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 950077.4915853041 },
-  { "Region": "Oxnard, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 862558.7976134975 },
-  { "Region": "Oxnard, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 870709.1354149851 },
-  { "Region": "Oxnard, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 888504.8877664494 },
-  { "Region": "Red Bluff, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 357100.8579760664 },
-  { "Region": "Red Bluff, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 368677.1328709362 },
-  { "Region": "Red Bluff, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 382796.1751807783 },
-  { "Region": "Redding, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 399641.9014312308 },
-  { "Region": "Redding, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 411034.948303994 },
-  { "Region": "Redding, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 421426.1860019147 },
-  { "Region": "Riverside, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 594106.1791962269 },
-  { "Region": "Riverside, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 603266.80904027 },
-  { "Region": "Riverside, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 614268.2408050784 },
-  { "Region": "Sacramento, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 585235.916842157 },
-  { "Region": "Sacramento, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 588903.6304548332 },
-  { "Region": "Sacramento, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 597713.2232530194 },
-  { "Region": "Salinas, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1002489.1232932205 },
-  { "Region": "Salinas, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1048112.4649676919 },
-  { "Region": "Salinas, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1086972.4154167867 },
-  { "Region": "San Diego, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 931649.3180720009 },
-  { "Region": "San Diego, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 949894.4354368546 },
-  { "Region": "San Diego, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 980019.216348591 },
-  { "Region": "San Francisco, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1512939.0644676283 },
-  { "Region": "San Francisco, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1523335.7288029946 },
-  { "Region": "San Francisco, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1559690.3491794157 },
-  { "Region": "San Jose, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1620375.8378740277 },
-  { "Region": "San Jose, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1666029.7564687666 },
-  { "Region": "San Jose, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1737387.3874375278 },
-  { "Region": "San Luis Obispo, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 922844.4222605332 },
-  { "Region": "San Luis Obispo, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 950394.1871781765 },
-  { "Region": "San Luis Obispo, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 974042.6711918215 },
-  { "Region": "San Rafael, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1457083.4652364331 },
-  { "Region": "San Rafael, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1464041.3470414227 },
-  { "Region": "San Rafael, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1508639.9594195881 },
-  { "Region": "Santa Cruz, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1214801.0841576376 },
-  { "Region": "Santa Cruz, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1245598.9472668061 },
-  { "Region": "Santa Cruz, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1301589.7499549387 },
-  { "Region": "Santa Maria, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 1090451.5954545953 },
-  { "Region": "Santa Maria, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 1160522.6606819325 },
-  { "Region": "Santa Maria, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 1221809.6169735759 },
-  { "Region": "Santa Rosa, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 808480.3712832563 },
-  { "Region": "Santa Rosa, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 813998.2069052551 },
-  { "Region": "Santa Rosa, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 829053.4213332868 },
-  { "Region": "Sonora, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 414581.8713437347 },
-  { "Region": "Sonora, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 424669.5902660995 },
-  { "Region": "Sonora, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 439121.0898689575 },
-  { "Region": "Stockton, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 534625.9457642603 },
-  { "Region": "Stockton, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 538559.626150624 },
-  { "Region": "Stockton, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 545792.1354345271 },
-  { "Region": "Susanville, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 246737.6224342978 },
-  { "Region": "Susanville, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 252897.1448779716 },
-  { "Region": "Susanville, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 253871.1756951626 },
-  { "Region": "Truckee, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 665930.8576890564 },
-  { "Region": "Truckee, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 680718.9854875725 },
-  { "Region": "Truckee, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 696627.858110579 },
-  { "Region": "Ukiah, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 490762.9839708847 },
-  { "Region": "Ukiah, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 484492.1651707669 },
-  { "Region": "Ukiah, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 480849.7664531444 },
-  { "Region": "Vallejo, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 591162.5406501322 },
-  { "Region": "Vallejo, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 595180.2439811548 },
-  { "Region": "Vallejo, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 602196.370567415 },
-  { "Region": "Visalia, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 390064.2857078727 },
-  { "Region": "Visalia, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 401396.2243558643 },
-  { "Region": "Visalia, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 411820.8092180408 },
-  { "Region": "Yuba City, CA metro area", "Year": 2026, "Avg Predicted Median Sale Price": 435919.6749864012 },
-  { "Region": "Yuba City, CA metro area", "Year": 2027, "Avg Predicted Median Sale Price": 439103.9077805041 },
-  { "Region": "Yuba City, CA metro area", "Year": 2028, "Avg Predicted Median Sale Price": 444367.5666839379 }
+  // Load from the new housing_forecast_2026_2028.json file
 ];
 
 const statesList = [
@@ -336,6 +227,7 @@ export default function Forecasting() {
   const [isLoading, setIsLoading] = useState(false);
   const [selectedCounties, setSelectedCounties] = useState([]);
   const [processedCounties, setProcessedCounties] = useState([]);
+  const [nyForecastData, setNyForecastData] = useState([]);
 
   useEffect(() => {
     if (searchQuery.length > 0) {
@@ -350,6 +242,20 @@ export default function Forecasting() {
     }
   }, [searchQuery]);
 
+  useEffect(() => {
+    // Load NY forecast data
+    const loadNyData = async () => {
+      try {
+        const response = await fetch('/data/housing_forecast_2026_2028.json');
+        const data = await response.json();
+        setNyForecastData(data);
+      } catch (error) {
+        console.error('Error loading NY forecast data:', error);
+      }
+    };
+    loadNyData();
+  }, []);
+
   const handleStateSelect = (state) => {
     setSelectedState(state);
     setSearchQuery(state);
@@ -358,10 +264,10 @@ export default function Forecasting() {
 
     // Simulate API call delay
     setTimeout(() => {
-      if (state.toLowerCase() === 'california') {
-        setForecastData(californiaData);
+      if (state.toLowerCase() === 'new york') {
+        setForecastData(nyForecastData);
         // Process data for county selection
-        const processed = processCountyData(californiaData);
+        const processed = processCountyData(nyForecastData);
         setProcessedCounties(processed);
         // Auto-select top 3 most expensive for demo
         const top3 = processed.sort((a, b) => b[2028] - a[2028]).slice(0, 3);
@@ -370,6 +276,7 @@ export default function Forecasting() {
         // For other states, show no data message
         setForecastData([]);
         setProcessedCounties([]);
+        setSelectedCounties([]);
       }
       setIsLoading(false);
     }, 1000);
@@ -389,7 +296,7 @@ export default function Forecasting() {
     data.forEach(item => {
       const region = item.Region;
       if (!regionData[region]) {
-        regionData[region] = { region, 2026: 0, 2027: 0, 2028: 0 };
+        regionData[region] = { region: region, 2026: 0, 2027: 0, 2028: 0 };
       }
       regionData[region][item.Year] = item['Avg Predicted Median Sale Price'];
     });
@@ -410,7 +317,7 @@ export default function Forecasting() {
     ];
 
     const datasets = selectedCounties.map((county, index) => ({
-      label: county.region.replace(', CA metro area', ''),
+      label: county.region.replace(', NY metro area', '').replace(', NY', ''),
       data: [county[2026], county[2027], county[2028]],
       borderColor: colors[index % colors.length],
       backgroundColor: colors[index % colors.length] + '20',
@@ -487,7 +394,7 @@ export default function Forecasting() {
           {/* Header */}
           <div className="text-center mb-12">
             <h1 className="text-4xl sm:text-5xl font-bold text-white mb-6">
-              Housing Price <span className="gradient-text">Forecasting</span>
+              {selectedState} Housing Price Forecasts
             </h1>
             <p className="text-xl text-gray-300 max-w-3xl mx-auto leading-relaxed">
               Explore predicted median home prices for metro areas across the United States through 2028
@@ -616,7 +523,7 @@ export default function Forecasting() {
                           .sort((a, b) => b[2028] - a[2028])
                           .map((county) => {
                             const growth = ((county[2028] - county[2026]) / county[2026] * 100);
-                            const region = county.region.replace(', CA metro area', '');
+                            const region = county.region.replace(', NY metro area', '').replace(', NY', '');
                             return (
                               <tr key={region} className="border-b border-gray-800 hover:bg-gray-800/30">
                                 <td className="py-3 px-4 text-white font-medium">{region}</td>
@@ -648,7 +555,7 @@ export default function Forecasting() {
                     No forecast data available for {selectedState}
                   </div>
                   <div className="text-gray-500 text-sm">
-                    Currently, we only have forecast data for California. More states coming soon!
+                    Currently, we only have forecast data for New York. More states coming soon!
                   </div>
                 </div>
               )}

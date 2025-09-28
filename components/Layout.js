db4cb6aa1838f51e0df595d4a6fa9b958c667738
rayanrehman4 <82070161+rayanrehman4@users.noreply.github.com>
2025-09-27 22:29:08 -0400

    Updated Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,6 @@ const Layout = ({ children, title = 'Affordly', description = 'Housing Affordabi
     { name: 'Forecasting', href: '/forecasting' },
     { name: 'Methodology', href: '/methodology' },
     { name: 'About', href: '/about' },
-    { name: 'Contact', href: '/contact' },
   ];
 
   return (
